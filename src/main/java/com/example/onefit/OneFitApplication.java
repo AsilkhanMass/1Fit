9bd763c0bd82e9628b3d7d5152a38b7aa292ea75
Asilkhan Abdumaminov <asilkhan566766@gmail.com>
2024-06-22 16:15:27 +0500

    init project

---
+++ src/main/java/com/example/onefit/OneFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.onefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onefit/OneFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
